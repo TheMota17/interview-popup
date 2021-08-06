f6fbb5ba3491ae669aa1ee3fb185bd6cc32d7cda
The Mota <ran777dom@gmail.com>
2021-08-06 23:43:24 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <FirstButton text="Досрочный платеж" />
+    <popup />
+  </main>
+</template>
+
+<script>
+import FirstButton from "@/components/FirstButton.vue";
+import Popup from "@/components/Popup.vue";
+
+export default {
+  components: {
+    FirstButton,
+    Popup,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  width: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: linear-gradient(to right, #FF4F4F, #DC3131);
+}
+</style>
---
+++ src/components/FirstButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button class="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+  },
+};
+</script> 
+
+<style lang="scss" scoped>
+.button {
+  border: 1px solid #fff;
+  outline: none;
+  border-radius: 6px;
+
+  padding: 1em;
+
+  font-size: 16px;
+  font-weight: 400;
+
+  color: #fff;
+  background: #FF5E56;
+
+  transition: 0.3s linear;
+
+  &:hover {
+    cursor: pointer;
+
+    color: #FF5E56;
+    background: #fff;
+  }
+
+  &:disabled {
+    cursor: default;
+
+    border: none;
+
+    background: #BEC5CC;
+  }
+}
+</style>
